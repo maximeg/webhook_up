ed43bc02931b3ae47a6889f3a100d39f074f9d2f
Maxime Garcia <maxime.garcia@gmail.com>
2016-08-15 10:07:21 +0200

    Spec: Set VERSION specs in its own file

---
+++ spec/webhook_up/version_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe WebhookUp::VERSION do
+  subject { WebhookUp::VERSION }
+
+  it 'exists' do
+    expect(subject).not_to be(nil)
+  end
+
+  it 'has the right format' do
+    expect(subject).to match(/\A\d+\.\d+\.\d+\z/)
+  end
+end
